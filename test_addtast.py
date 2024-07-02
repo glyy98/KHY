18ac60e88af4682826978f35568953583c334013
glyy98 <1007452825@qq.com>
2024-07-02 10:38:51 +0800

    添加了一些注释

--- test_addtast.py
+++ test_addtast.py
@@ -3,31 +3,31 @@ import requests
 from test_login import login
 
 
-@pytest.fixture
+@pytest.fixture    #装饰器核心在于减少重复代码，调用登录函数返回token
 def token():
     return login()
 
 def test_addtask(token):
-    base_url='https://dev.mhiiot.cn/openapi/project/api'  
-    add_task_url=f'{base_url}/TaskTable/add'
+    base_url='https://dev.mhiiot.cn/openapi/project/api'   #基础地址
+    add_task_url=f'{base_url}/TaskTable/add'  #基础地址+接口路径
 
     headers={'Content-Type':'application/json',
-             'Authorization':f'Bearer {token}'}
-    
+             'Authorization':f'Bearer {token}'}  #这个Bearer在加token时需要空格
+    #传参
     task_data={
   "taskType": 3,   #维修
   "deviceDynamicInfoId": [ "495096115801096261"],  #设备小类
-  "maintenanceContent": "接口自动化",   #检修内容
+  "maintenanceContent": "调用测试",   #检修内容
   "maintenanceMethods": "",
   "maintenanceStandards": "",
-  "maintenanceTime": "2024-06-28 00:00:00",  #检修时间
+  "maintenanceTime": "2024-07-03 00:00:00",  #检修时间
   "leader": ["558143240922988613"],  #负责人
   "taskStatus": 1,   #进行中
   "craneId": "421471904994426949",   #起重机id
   "remark": ""   }#自定义设备
     
     response=requests.post(add_task_url,json=task_data,headers=headers)
-
+    #校验响应数据的状态码是否为200，否则输出提示
     assert response.status_code==200,f'添加任务失败,{response.text}'
 
     response_data=response.json()
--- test_login.py
+++ test_login.py
@@ -1,25 +1,24 @@
 import requests
-import pytest
-
+#登录函数
 def login():
     #基础地址
     base_url='https://dev.mhiiot.cn/openapi/client/api'  
     #基础地址+接口名
     url=f'{base_url}/login'  
-    #定义请求体  字典形式
+    #定义请求体  字典形式  账号密码
     zhanghao={'username':"superadmin",'password':'123456'}
     #定义请求头  
     headers={"Content-Type":"application/json"}
 
-    #发送post请求
+    #发送post请求  其实就是将所有条件打包
     response=requests.post(url,headers=headers,json=zhanghao)
     response.raise_for_status()  #如果状态码不是200就会报httperror
-    #获取响应数据
+    #获取响应数据  
     response_data=response.json()
     #字典使用get方法获取键值，如果键值不存在也不会报错，会返回none
     accessToken=response_data['data'].get('accessToken',None)
     if accessToken is None:
         raise ValueError("您的登录未获取到token")
 
-    return accessToken
+    return accessToken    #该函数返回token
 
